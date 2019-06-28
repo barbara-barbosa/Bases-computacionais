3ca3347208f941aeb47e828195e4c16950fbd7c2
Bárbara Barbosa <51497274+barbara-barbosa@users.noreply.github.com>
2019-06-28 20:57:50 -0300

    Add files via upload

---
+++ Tarefa3Barbara.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 20:30:54 2019
+
+@author: barbara.barbosa
+"""
+"""
+Este programa calcular o número de segundos s, dado o número de horas 
+"""
+
+
+
+x=1.5
+y=12
+z=24
+s=3600
+print('1 hora e meia equivale =',(x*s),'s')
+print('12 horas equivale  =',(y*s),'s')
+print('24 horas equivale  =',(z*s),'s')
