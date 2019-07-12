868d1eaf13cd0fb5fb591eec0476cac1a48b2bf1
Bárbara Barbosa <51497274+barbara-barbosa@users.noreply.github.com>
2019-07-12 20:24:43 -0300

    Add files via upload

---
+++ Tarefa7Barbara.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 20:30:54 2019
+
+@author: barbara.barbosa
+"""
+"""
+Faça um programa que mostre se o número inteiro guardado na variável x é par ou ímpar.
+ Caso seja par, deve aparecer na tela: "O número x é par". 
+ Caso seja ímpar deve aparecer "O número x é ímpar".
+Teste para a) x = 1, b) x = 24, c) x = 10, d) x = 5.
+"""
+
+a=1
+s=a%2
+
+if s==0:
+    print("O número",a,"é par")
+    
+else:
+    print("O número",a,"é impar")
+    
+b=24
+s=b%2
+
+if s==0:
+    print("O número",b,"é par")
+    
+else:
+    print("O número",b,"é impar")
+    
+c=10
+s=c%2
+
+if s==0:
+    print("O número",c,"é par")
+    
+else:
+    print("O número",c,"é impar")
+         
+d=5
+s=d%2
+
+if s==0:
+    print("O número",d,"é par")
+    
+else:
+    print("O número",d,"é impar")
+
+    
